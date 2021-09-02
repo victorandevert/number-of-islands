9900c6738cd1a709e1b03a6aa67af0c97d47cc45
Victor Andevert <victor.andevert@gmail.com>
2021-09-02 20:20:16 +0200

    return number of islands

--- src/main/kotlin/Map.kt
+++ src/main/kotlin/Map.kt
@@ -1,3 +1,39 @@
+
 fun `number of islands`(grid: Array<CharArray>):Int {
-    return 1
+    val visitedNodes = grid.copyOf()
+        .map { CharArray(it.size){'-'} }
+        .toTypedArray()
+    var islands = 0
+    for (row in grid.indices){
+        for (column in 0 until grid[row].size) {
+            if ((row == 0 || row < grid.size || column==0 || column<grid[row].size) && grid[row][column] == '1' && visitedNodes[row][column] != '1') {
+                visitedNodes[row][column] = '1'
+                islands = inspectNode(row, column, grid, visitedNodes, islands)
+            }
+        }
+    }
+    return islands
+}
+
+fun inspectNode(row: Int, column: Int, grid: Array<CharArray>, visitedNodes: Array<CharArray>, islands: Int): Int {
+
+    if (row+1<grid.size && grid[row+1][column]=='1' && visitedNodes[row+1][column] != '1') {
+        visitedNodes[row+1][column]='1'
+        inspectNode(row+1, column, grid, visitedNodes, islands)
+    }
+    if (column+1<grid[row].size && grid[row][column+1]=='1' && visitedNodes[row][column+1] != '1') {
+        visitedNodes[row][column+1]='1'
+        inspectNode(row, column+1, grid, visitedNodes, islands)
+    }
+    if (row-1>=0 && grid[row-1][column]=='1' && visitedNodes[row-1][column] != '1') {
+        visitedNodes[row-1][column]='1'
+        inspectNode(row-1, column, grid, visitedNodes, islands)
+    }
+    if (column-1>=0 && grid[row][column-1]=='1' && visitedNodes[row][column-1] != '1') {
+        visitedNodes[row][column-1]='1'
+        inspectNode(row, column-1, grid, visitedNodes, islands)
+    }
+
+   return islands+1
+
 }
--- src/test/kotlin/WorldShould.kt
+++ src/test/kotlin/WorldShould.kt
@@ -1,12 +1,50 @@
-import org.assertj.core.api.Assertions.*
-import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
+@TestInstance(PER_CLASS)
 class WorldShould {
 
-    @Test
-    fun `find islands`() {
-        assertThat(`number of islands`(arrayOf(
-            charArrayOf('1'),
-        ))).isEqualTo(1)
+    @ParameterizedTest
+    @MethodSource("grids")
+    fun `find islands`(grid: Array<CharArray>, islands: Int) {
+        assertThat(`number of islands`(grid)).isEqualTo(islands)
+    }
+
+    private fun grids(): Stream<Arguments> {
+        return Stream.of(
+            Arguments.of(arrayOf(charArrayOf('1')), 1),
+            Arguments.of(arrayOf(charArrayOf('1','1')), 1),
+            Arguments.of(arrayOf(charArrayOf('1','1','0','1')), 2),
+            Arguments.of(arrayOf(
+                charArrayOf('1','0'),
+                charArrayOf('0','1')), 2),
+            Arguments.of(arrayOf(
+                charArrayOf('1','1','1','1','0'),
+                charArrayOf('1','1','0','1','0'),
+                charArrayOf('1','1','0','0','0'),
+                charArrayOf('0','0','0','0','0')), 1),
+            Arguments.of(arrayOf(
+                charArrayOf('1','1','0','0','0'),
+                charArrayOf('1','1','0','0','0'),
+                charArrayOf('0','0','1','0','0'),
+                charArrayOf('0','0','0','1','1')), 3),
+            Arguments.of(arrayOf(
+                charArrayOf('1','0','0','0','0','0'),
+                charArrayOf('0','1','0','1','1','1'),
+                charArrayOf('0','0','1','0','1','0'),
+                charArrayOf('1','1','0','0','1','0'),
+                charArrayOf('1','0','1','1','0','0'),
+                charArrayOf('1','0','0','0','0','1')), 7),
+            Arguments.of(arrayOf(
+                charArrayOf('1','0','1','0','0'),
+                charArrayOf('1','0','0','0','0'),
+                charArrayOf('0','0','1','0','0'),
+                charArrayOf('0','0','0','1','1')), 4),
+        )
     }
 }
