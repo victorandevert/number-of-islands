956cb3a86ad1d341a40d12ff2ac7566b88e23499
Victor Andevert <victor.andevert@gmail.com>
2021-09-03 11:56:37 +0200

    rename and remove unnecessary method

--- src/main/kotlin/Map.kt
+++ src/main/kotlin/Map.kt
@@ -8,7 +8,7 @@ fun numberOfIslands(grid: Array<CharArray>):Int {
     grid.indices.forEach { row ->
        grid[row].indices.forEach { column ->
            when {
-               isBorderNode(row, column, grid) && isValidNode(row, column, grid) && isUnvisitedNode(row, column, visitedNodes) ->
+               isValidNode(row, column, grid, visitedNodes) ->
                    islands = inspectNode(row, column, grid, visitedNodes, islands)
            }
         }
@@ -21,7 +21,7 @@ private fun inspectNode(row: Int, column: Int, grid: Array<CharArray>, visitedNo
         val newRow = it.row + row
         val newColumn = it.column + column
         when {
-            isNeighbor(newRow, newColumn, grid, visitedNodes) -> {
+            isValidNode(newRow, newColumn, grid, visitedNodes) -> {
                 addVisitedNode(newRow, newColumn, visitedNodes)
                 inspectNode(newRow, newColumn, grid, visitedNodes, islands)
             }
@@ -36,14 +36,16 @@ private fun addVisitedNode(newRow: Int, newColumn: Int, visitedNodes: Array<Char
 private fun isBorderNode(row: Int, column: Int, grid: Array<CharArray>) =
     row >= 0 && column >= 0 && row < grid.size && column < grid[row].size
 
-private fun isValidNode(row: Int, column: Int, grid: Array<CharArray>) = grid[row][column] == '1'
+private fun isValidValue(row: Int, column: Int, grid: Array<CharArray>) = grid[row][column] == '1'
 
-private fun isUnvisitedNode(row: Int, column: Int, grid: Array<CharArray>) = !isValidNode(row, column, grid)
+private fun isUnvisitedNode(row: Int, column: Int, grid: Array<CharArray>) = !isValidValue(row, column, grid)
 
-private fun isNeighbor(newRow: Int, newColumn: Int, grid: Array<CharArray>, visitedNodes: Array<CharArray>) =
-    isBorderNode(newRow, newColumn, grid) &&
-    isValidNode(newRow, newColumn, grid) &&
-    isUnvisitedNode(newRow, newColumn, visitedNodes)
+private fun isValidNode(
+    row: Int,
+    column: Int,
+    grid: Array<CharArray>,
+    visitedNodes: Array<CharArray>
+) = isBorderNode(row, column, grid) && isValidValue(row, column, grid) && isUnvisitedNode(row, column, visitedNodes)
 
 enum class Position(val row: Int, val column: Int){
     UP(-1,0),
